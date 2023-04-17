cd4a7b707e9478898c68efffefde53bc92e91bd3
sander-cohen <130958732+sander-cohen@users.noreply.github.com>
2023-04-17 11:27:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import pandas as pd
+
 import numpy as np
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 # Credit Card Approval Prediction App
